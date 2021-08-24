c77ea5323d44928d643474bcdbcf2fa99bddc5a2
Damian <szkola_abc@o2.pl>
2021-08-24 18:31:35 +0200

    Added BaseController and ServiceResponse template

---
+++ GradeManagement/API/Controllers/BaseController.cs
@@ -0,0 +1,49 @@
+﻿using Application.Services.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult SendResponse(ServiceResponse response)
+        {
+            switch (response.ResponseType)
+            {
+                case HttpStatusCode.OK:
+                    return Ok();
+                case HttpStatusCode.Unauthorized:
+                    return Unauthorized();
+                case HttpStatusCode.Forbidden:
+                    return Forbid();
+                case HttpStatusCode.NotFound:
+                    return NotFound();
+                default:
+                    return BadRequest(response.Message);
+            }
+        }
+
+        protected IActionResult SendResponse<T>(ServiceResponse<T> response)
+        {
+            switch (response.ResponseType)
+            {
+                case HttpStatusCode.OK:
+                    return Ok(response.ResponseContent);
+                case HttpStatusCode.Unauthorized:
+                    return Unauthorized();
+                case HttpStatusCode.Forbidden:
+                    return Forbid();
+                case HttpStatusCode.NotFound:
+                    return NotFound();
+                default:
+                    return BadRequest(response.Message);
+            }
+        }
+    }
+}
---
+++ GradeManagement/Application/Services/Utilities/ServiceResponse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Utilities
+{
+    public class ServiceResponse
+    {
+        public HttpStatusCode ResponseType { get; set; }
+        public string Message { get; set; }
+
+        public ServiceResponse(HttpStatusCode responseType)
+        {
+            ResponseType = responseType;
+        }
+
+        public ServiceResponse(HttpStatusCode responseType, string message)
+        {
+            ResponseType = responseType;
+            Message = message;
+        }
+    }
+
+    public class ServiceResponse<T>
+    {
+        public HttpStatusCode ResponseType { get; set; }
+        public string Message { get; set; }
+        public T ResponseContent { get; set; }
+
+        public ServiceResponse(HttpStatusCode responseType)
+        {
+            ResponseType = responseType;
+        }
+
+        public ServiceResponse(HttpStatusCode responseType, string message)
+        {
+            ResponseType = responseType;
+            Message = message;
+        }
+
+        public ServiceResponse(HttpStatusCode responseType, T responseContent)
+        {
+            ResponseType = responseType;
+            ResponseContent = responseContent;
+        }
+
+        public ServiceResponse(HttpStatusCode responseType, string message, T responseContent)
+        {
+            ResponseType = responseType;
+            Message = message;
+            ResponseContent = responseContent;
+        }
+    }
+}
